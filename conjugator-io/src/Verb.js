005ad9cf977817a59bef4f699b239058752f5c9f
webbmaxwell <webbwrites@gmail.com>
2019-08-26 21:33:52 -0600

    Finished building Verb component. Started rendering with placeholders for state

--- conjugator-io/src/Verb.js
+++ conjugator-io/src/Verb.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Verb() {
+
+  const [verb, setVerb] = useState('Infinitive Verb');
+  const [def, setDef] = useState('definition');
+  const [tense, setTense] = useState('Tense');
+
   return (
-    <>
-    </>
+    <div className="verb">
+      <p>Verb:</p>
+      <div className="verb-def">
+        <h3>{verb}</h3>
+        <p className="definition">({def})</p>
+      </div>
+      <p>Tense:</p>
+      <h3>{tense}</h3>
+    </div>
   )
 }
 
