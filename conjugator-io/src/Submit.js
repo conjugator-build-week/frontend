729f582fc8533bc657117bc17602ba80ee2e0e29
webbmaxwell <webbwrites@gmail.com>
2019-08-26 21:39:52 -0600

    Started building Submit component.

--- conjugator-io/src/Submit.js
+++ conjugator-io/src/Submit.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Submit() {
+
+  const [person, setPerson] = useState('Nosotros');
+
   return (
-    <>
-    </>
+    <div className="submit">
+      <form className="verb-entry">
+        <p>{person}</p>
+        <input type="text" placeholder="Enter conjugated verb..." />
+      </form>
+    </div>
   )
 }
 
