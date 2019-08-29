f26a37c175d957e1862d30b0bf684b69440f0dc4
webbmaxwell <webbwrites@gmail.com>
2019-08-28 19:34:03 -0600

    Successfully made the API call but not rendering the verbs correctly. Continuing to debug this

--- conjugator-io/src/Verb.js
+++ conjugator-io/src/Verb.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Verb() {
+
+  function getRandom(max) {
+    return Math.floor(Math.random()*Math.floor(max));
+  }
 
   const [verb, setVerb] = useState('Infinitive Verb');
   const [def, setDef] = useState('definition');
   const [tense, setTense] = useState('Tense');
+
+  useEffect(() => {
+    axios.get("https://raw.githubusercontent.com/ghidinelli/fred-jehle-spanish-verbs/master/jehle_verb_lookup.json")
+    .then(res => {
+      const verbs = Object.entries(res.data);
+      console.log(verbs);
+      setVerb(verb)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className="verb">
