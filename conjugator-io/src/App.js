18b78469b77101a93bf02bc74423344a1d9d66a4
unknown <toosdai52Otte@gmail.com>
2019-08-29 21:46:00 -0500

    homepage components on page, login modal working, Routes set, signup page and some style added

--- conjugator-io/src/App.js
+++ conjugator-io/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
+import LoginModal from './LoginModal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/login" component={LoginModal} />
     </div>
   );
 }
---
+++ conjugator-io/src/LoginModal.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Container, Modal } from 'semantic-ui-react';
+import LoginForm from './LoginForm';
+
+class LoginModal extends Component {
+    state = { open: false }
+    
+    show = (dimmer) => this.setState({ dimmer, open: true })
+    close = () => this.setState({ open: false})
+    
+    render(){
+        const { open, dimmer } = this.state;
+
+        return(
+            <Container style={{width: '30%'}}>
+                {/* Login Button */}
+                <div name="login" onClick={this.show}>
+                    Login
+                </div>
+
+
+                <Modal 
+                    dimmer={dimmer} 
+                    open={open} 
+                    onClose={this.close}
+                    style={{width: "40%"}}
+                >
+                    <Modal.Header style={{textAlign: "center"}}>Login</Modal.Header>
+                    <Modal.Content>
+                        <LoginForm />
+                    </Modal.Content>
+                </Modal>
+
+            </Container>
+        )
+    }
+};
+
+export default LoginModal;
